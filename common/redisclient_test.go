79dc8542d2748955f26b4997248588aaccf496e2
ChainChainC <1604956393@qq.com>
2023-06-11 22:52:51 +0900

    完善redis，set、string、hash的写入读取的测试。

--- common/redisclient_test.go
+++ common/redisclient_test.go
@@ -10,6 +10,8 @@ import (
 	"time"
 )
 
+// ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)可以设置redis操作的超时时间
+
 func TestBase(t *testing.T) {
 	NewRedisClientApp()
 	defer LocalRedisClient.Client.Close()
@@ -83,5 +85,55 @@ func TestSet(t *testing.T) {
 		t.Fail()
 	} else {
 		fmt.Printf("删除数量：%d", count)
+	}
+}
+
+type HashUpdate struct {
+	PlayerType int8
+	RoomId     string
+	Sight      int
+}
+
+func TestHash(t *testing.T) {
+	NewRedisClientApp()
+	defer LocalRedisClient.Client.Close()
+	// 用户更新key: Player:uuid map[string]interface{}
+	key := "Player:uuid1"
+	// 如果直接结构体，插入会报错
+	// val := &HashUpdate{
+	// 	PlayerType: 1,
+	// 	RoomId:     "Aaaaaaaa",
+	// 	Sight:      100,
+	// }
+	val := map[string]interface{}{
+		"PlayerType": 1,
+		"RoomId":     "Aaaaaaaa",
+		"Sight":      100,
+	}
+	// 需要在初始化时检测是否连接成功
+	ctx := context.Background()
+	// HSet插入数据--->只能单个 key val插入
+	// HMSet，使用map进行插入
+	if err := LocalRedisClient.Client.HMSet(ctx, key, val).Err(); err != nil {
+		fmt.Printf("数据插入错误")
+		t.Fail()
+	} else {
+		fmt.Printf("数据写入成功")
+	}
+	// HMGET, 根据hash key和多个字段获取值
+	if vals, err := LocalRedisClient.Client.HMGet(ctx, key, "RoomId").Result(); err != nil {
+		fmt.Printf("数据获取错误")
+		t.Fail()
+	} else {
+		fmt.Println(vals)
+	}
+	// HLen，获取hashkey的字段多少
+	// HDel，删除字段，支持删除多个字段
+
+	// 设置键的过期时间为 120 秒
+	err := LocalRedisClient.Client.Expire(ctx, key, 120*time.Second).Err()
+	if err != nil {
+		fmt.Println("Failed to set expiration:", err)
+		t.Fail()
 	}
 }
