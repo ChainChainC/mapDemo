7d6297c87bfad9fbe83ffc67ee98dd0c05ea5bcc
ChainChainC <1604956393@qq.com>
2023-07-10 17:09:00 +0800

    --init main

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	common.NewRedisClientApp()
+}
+
 func main() {
 
 	r := gin.Default()
 	r = CollectRoute(r)
-	common.NewRedisClientApp()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
